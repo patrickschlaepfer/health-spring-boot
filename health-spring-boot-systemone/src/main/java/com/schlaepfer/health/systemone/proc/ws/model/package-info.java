d5b69e13bd29520959772f055011e2e113e6c1ec
Patrick Schlaepfer <patrick@schlaepfer.com>
2016-06-09 19:12:40 +0200

    Some naming fixed

--- health-spring-boot-systemone/src/main/java/com/schlaepfer/health/systemone/proc/ws/model/package-info.java
+++ health-spring-boot-systemone/src/main/java/com/schlaepfer/health/systemone/proc/ws/model/package-info.java
@@ -1,3 +1,3 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.eu.hcnet.biz/increment", 
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.schlaepfer.com/increment", 
 	elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package com.schlaepfer.health.systemone.proc.ws.model;
